65739a84d02140da7805f94af3bdeb128db9dd72
CDelerme7673 <cdelerme7673@gmail.com>
2025-08-15 15:38:07 -0400

    first commit

---
+++ src/main/java/com/tutorial/ecommercerestapi/EcommerceRestApiApplication.java
@@ -0,0 +1,14 @@
+package com.tutorial.ecommercerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceRestApiApplication.class, args);
+        System.out.println("EcommerceRestApiApplication started");
+    }
+
+}
